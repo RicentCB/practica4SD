4f9d2337fc0f016cf0243f8bdf323bf2ad1010db
jedel1043 <jedel0124@gmail.com>
2021-06-08 00:42:22 -0500

    Desactiva devtools

--- server/main.js
+++ server/main.js
@@ -24,7 +24,7 @@ function createWindow() {
     mainWindow.setMenuBarVisibility(false);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
