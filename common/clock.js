34d4c412b5c3a708c347bfcda00d622c56d0b6e2
jedel1043 <jedel0124@gmail.com>
2021-06-08 01:56:09 -0500

    Arregla bug de clock

--- common/clock.js
+++ common/clock.js
@@ -4,21 +4,14 @@ const dayInMillis = 24 * 60 * 60 * 1000;
 const hourInMillis = 60 * 60 * 1000;
 const minuteInMillis = 60 * 1000;
 
-function mod(n, b) {
-    if (n > b) {
-        return n - b;
-    } else {
-        return n;
-    }
-}
 
 export default class Clock {
     constructor(hours, mins, secs, millis) {
         let now = new Date();
-        hours = mod((hours ?? now.getHours()), 24);
-        mins = mod((mins ?? now.getMinutes()), 60);
-        secs = mod((secs ?? now.getSeconds()), 60);
-        millis = mod((millis ?? now.getMilliseconds()), 1000);
+        hours = (hours ?? now.getHours()) % 24;
+        mins = (mins ?? now.getMinutes()) % 60;
+        secs = (secs ?? now.getSeconds()) % 60;
+        millis = (millis ?? now.getMilliseconds()) % 1000;
         this._millis = hours * hourInMillis + mins * minuteInMillis + secs * 1000 + millis;
     }
     get time() {
@@ -39,15 +32,15 @@ export default class Clock {
     };
 
     set time(newTime) {
-        newTime.hours = mod(newTime.hours, 24);
-        newTime.mins = mod(newTime.mins, 60);
-        newTime.secs = mod(newTime.secs, 60);
-        newTime.millis = mod(newTime.millis, 1000);
+        newTime.hours = newTime.hours % 24;
+        newTime.mins = newTime.mins % 60;
+        newTime.secs = newTime.secs % 60;
+        newTime.millis = newTime.millis % 1000;
         this._millis = newTime.hours * hourInMillis + newTime.mins * minuteInMillis + newTime.secs * 1000 + newTime.millis;
     }
     advance(millis) {
         millis = millis ?? 1;
-        this._millis = mod(this._millis + millis, dayInMillis);
+        this._millis = (this._millis + millis + dayInMillis) % dayInMillis;
     }
 }
 
